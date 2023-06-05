153c3ec3e1fe47219309befe6fd98a706763e470
Gyan Lakhwani <gyanlakhwani@gmail.com>
2023-06-05 17:35:15 +0530

    commit 1

---
+++ script.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('.tab').forEach(item => {
+    item.addEventListener('click', event => {
+        document.querySelectorAll('.tab').forEach(item => {
+            item.classList.remove('active');
+        });
+        event.target.classList.add('active');
+    })
+})
+
+// document.getElementById("bold").addEventListener("click", function() {
+//     var editor = document.getElementById("editor");
+//     var selection = editor.getSelection();
+//     var range = selection.getRangeAt(0);
+
+//     var newNode = document.createElement("b");
+//     newNode.appendChild(range.extractContents());
+//     range.insertNode(newNode);
+
+//     selection.removeAllRanges();
+//     selection.addRange(range);
+//   });
+
+
+
+
+
+
+
+
